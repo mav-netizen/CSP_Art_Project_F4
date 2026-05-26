3d2ed68c77dca6d5db3d64fc1c118a944ad3f6c2
pnguyen29-art <pnguyen29@ssis.edu.vn>
2026-05-26 10:27:45 +0700

    my_picture.py

--- my_picture.py
+++ my_picture.py
@@ -8,10 +8,18 @@ def draw_picture(width, height):
     
     # make some variables available
     colors = ["red", "green", "blue", "cyan", "magenta", "yellow"]
+    sg.set_outline_color("green")
     sg.set_fill_color ("green")
     sg.fill_rectangle(0, 280,600, 400)
+    sg.set_fill_color ("blue")
+    sg.set_outline_color("blue")
+    sg.fill_rectangle(450, 280,100, 25)
+    sg.fill_rectangle(425, 300,100, 25)
+    sg.fill_rectangle(400, 320,100, 25)
+    sg.fill_rectangle(375, 340,100, 25)
+    sg.fill_rectangle(350, 360,100, 25)
+    sg.fill_rectangle(325, 380,100, 25)
 
-    
 
 if __name__ == "__main__":
     # Launch the wrapper; only edit starting dimensions of canvas if you would like to
